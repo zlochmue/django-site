dcf8193152b83d01a7a435dbe858473700055578
Zachary Lochmueller <95509424+zlochmue@users.noreply.github.com>
2022-08-01 19:20:32 -0500

    updated views.py
    added call to Get endpoint

--- blog/views.py
+++ blog/views.py
@@ -5,20 +5,11 @@ import boto3
 import os 
 
 def read_latest(request):
-    client = boto3.client('dynamodb',
-                        region_name='us-east-2',
-                        aws_access_key_id=os.getenv("AWS_ACCESS_KEY"),
-                        aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"))
-    db = boto3.resource('dynamodb', 
-                        region_name='us-east-2',
-                        aws_access_key_id=os.getenv('AWS_ACCESS_KEY'),
-                        aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'))   
-
-    table = db.Table('sensor')
-    response = table.scan()
-    items = response['Items']
-    items = sorted(items, reverse=True, key=lambda x: x['time'])
-    return render(request, 'blog/read_latest.html', { 'item': items[0] } )
+    url = 'https://jx7a1ot8db.execute-api.us-east-2.amazonaws.com/GetMostRecent'
+    response = urlopen(url)
+    data_json = json.loads(response.read())
+    item = data_json['body']                                                                                                      
+    return render(request, 'blog/read_latest.html', { 'item': item } )
 
 def post_list(request):
     posts = Post.objects.all()
